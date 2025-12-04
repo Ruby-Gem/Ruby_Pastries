e57c5937f48da761c64c304da5d239861f273c06
Elizabeth Ayangbola <107759458+Ruby-Gem@users.noreply.github.com>
2025-12-04 22:34:13 +0100

    improved responsiveness and added more page content

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,9 +1,35 @@
 
-const slides = document.querySelectorAll('.carousel-container .slides');
-
-document.querySelector('button[name="random"]').addEventListener('click', () => {
-    const targetSlide = slides[ Math.floor(Math.random() * slides.length) ]
-    targetSlide.scrollIntoView({
-        block: 'center'
-    })
+document.querySelectorAll(".mansory figure").forEach((fig) => {
+    fig.addEventListener("click", () => {
+        document
+        .querySelector("#modalImg")
+        .setAttribute("src", fig.getAttribute("data-src"));
+        document.querySelector("#modalCaption").textContent =
+        fig.getAttribute("data-caption");
+        document.querySelector("#imgModal").showModal();
+    });
 });
+
+const slides = document.querySelectorAll('.slide');
+
+function randomFunFact() {
+    slides[Math.floor(Math.random() * slides.length)].scrollIntoView({
+        block: 'center',
+        behavior: 'smooth'
+    })
+}
+
+document.querySelectorAll('[role="accordion-group"]').forEach(accordion => {
+    const details = accordion.querySelectorAll('details');
+    details.forEach(detail => {
+        detail.addEventListener('toggle', (event) => {
+            const toggleDetail = event.target;
+            if (toggleDetail.open) {
+                details.forEach(others => {
+                    if (others !== toggleDetail) others.open = false;
+                })
+            }
+        })
+    })
+})
+
